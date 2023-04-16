849de34d4415ce29dace3cb6533877d04a5d2ded
Nekorise <32713344+Bayori@users.noreply.github.com>
2023-04-16 14:05:42 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,23 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<storage> articleList = new ArrayList<>(); // Создание списка
+        List<storage> articleList = new ArrayList<>(); // РЎРѕР·РґР°РЅРёРµ СЃРїРёСЃРєР°
 
-        // Объекты с данными
-        storage article1 = new storage(1, "Статья про сыр", "Сыр - вкусно", List.of("cheese", "food"), new Date(105, 9, 10), true);
-        storage article2 = new storage(2, "Статья о луне", "Луна - дырки", List.of("space", "moon"), new Date(118, 0, 1), false);
-        storage article3 = new storage(3, "Статья про редкоземельные магниты", "Магниты - сильно", List.of("magnets", "ores"), new Date(127, 5, 15), true);
-        storage article4 = new storage(4, "Гайд по C++", "C++ - круто", List.of("programming", "c++"), new Date(134, 8, 22), false);
-        storage article5 = new storage(5, "Топ5 смешных фактов про салфетку", "Салфетки не смешные", List.of("napkins", "joke"), new Date(142, 2, 5), true);
+        // РћР±СЉРµРєС‚С‹ СЃ РґР°РЅРЅС‹РјРё
+        storage article1 = new storage(1, "РЎС‚Р°С‚СЊСЏ РїСЂРѕ СЃС‹СЂ", "РЎС‹СЂ - РІРєСѓСЃРЅРѕ", List.of("cheese", "food"), new Date(105, 9, 10), true);
+        storage article2 = new storage(2, "РЎС‚Р°С‚СЊСЏ Рѕ Р»СѓРЅРµ", "Р›СѓРЅР° - РґС‹СЂРєРё", List.of("space", "moon"), new Date(118, 0, 1), false);
+        storage article3 = new storage(3, "РЎС‚Р°С‚СЊСЏ РїСЂРѕ СЂРµРґРєРѕР·РµРјРµР»СЊРЅС‹Рµ РјР°РіРЅРёС‚С‹", "РњР°РіРЅРёС‚С‹ - СЃРёР»СЊРЅРѕ", List.of("magnets", "ores"), new Date(127, 5, 15), true);
+        storage article4 = new storage(4, "Р“Р°Р№Рґ РїРѕ C++", "C++ - РєСЂСѓС‚Рѕ", List.of("programming", "c++"), new Date(134, 8, 22), false);
+        storage article5 = new storage(5, "РўРѕРї5 СЃРјРµС€РЅС‹С… С„Р°РєС‚РѕРІ РїСЂРѕ СЃР°Р»С„РµС‚РєСѓ", "РЎР°Р»С„РµС‚РєРё РЅРµ СЃРјРµС€РЅС‹Рµ", List.of("napkins", "joke"), new Date(142, 2, 5), true);
 
-        // Добавление объектов в лист
+        // Р”РѕР±Р°РІР»РµРЅРёРµ РѕР±СЉРµРєС‚РѕРІ РІ Р»РёСЃС‚
         articleList.add(article1);
         articleList.add(article2);
         articleList.add(article3);
         articleList.add(article4);
         articleList.add(article5);
 
-        // Вывод
+        // Р’С‹РІРѕРґ
         System.out.println(articleList);
     }
 }
